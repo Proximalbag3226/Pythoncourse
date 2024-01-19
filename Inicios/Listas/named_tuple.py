a1298bcba28c8ec356942cc895d07fa6610c9d65
Luis Enrique <perezcoronaluisenrique4@gmail.com>
2024-01-19 02:27:44 -0600

    Inicio colecciones

---
+++ Inicios/Listas/named_tuple.py
@@ -0,0 +1,6 @@
+#A diferencia de su hermana la tupla que so bastante parecidas entre si eso si se debe mencionar esta nos permite trabajar co tuplas las cuales sus elementos tienen 
+#Nombres en particular
+import collections 
+import named_tuple
+
+Producto = named_tuple('Producto','Nombre Precio Cantidad')
