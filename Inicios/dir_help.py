66daab730c9fbf6190387a6d586073a7cdd5f8b6
Luis Enrique <perezcoronaluisenrique4@gmail.com>
2024-01-23 23:46:49 -0600

    Dunder inicios

--- Inicios/dir_help.py
+++ Inicios/dir_help.py
@@ -1,2 +1,11 @@
 #Los dir y help son comandos de la consola de pytho que ya viene integrados por defecto en el lenguaje y que nos sirven para obtener informacion hacerca de las 
 #Librerias que instalemos o que importemos siempre y cuando esta tenga la documentacion adecuada 
+#Ejemplo de comandos para obtener informacion de la libreria
+
+#Importamos la libreria 
+#import pandas as pd
+#Y pedimos informacion de ella
+#dir(pd)
+#dir(pd.value_counts)
+#help(pd)
+
