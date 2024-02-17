0e05ae9a60bea88b0a8e6623ec3ad53eba280ec8
Luis Enrique <perezcoronaluisenrique4@gmail.com>
2024-02-17 00:34:44 -0600

    Random inicios

---
+++ Inicios/random.py
@@ -0,0 +1,10 @@
+#Vamos a trabajar con random para la creacion de numeros aleatorios, random creo ya lo eh usado antes para otros codigos aqui pero no como tal explicarlo para que lo uses
+#Random es un modulo ya nativo de python por lo cuao no nesesita su instalacion o algo asi simplemente la importacion del modulo dentro del codigo
+#Randon se encarga de la generacion de numeros pseudoaleatorios para varias distribuciones, en la teoria este modulo utiliza diferentes mtodologias y formas para hacerlo
+#Posible el echo de calcular esos numeros, para los numeros enteros hay una seleccion uniforme dentro de un rango establecido, para las secuencias se utiliza 
+#Una seleccion uniforme de un elemento aleatorio, una funcion para generar permutaciones aleatorias de la lista y otra funcion para el muestreo aleatorio sin remplazo
+#Para los numeros reales el modulo tiene mas formas de tratarlo ya que existen diferentes formas para calcular distribuciones uniformes, normales(la de gauss), 
+#Logaritmicas normales y tambien de exponenciales negativas y por supuesto de las funciones gamma y beta. (Esto fuera de la programacion es interesante asi que podrias repasarlo
+#En los libros de calculo y calculo avanzado)
+#Ahora si vengan los ejemplos 
+import random
